5bc814ddd1b29d693dc43e772a1392afa765e8a4
Maxim Tishchenko <qwertmax@gmail.com>
2015-12-10 00:19:11 +0600

    try to deploy 9

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func CityHandler(res http.ResponseWriter, req *http.Request) {
 	res.Write(data)
 }
 
-// try to deploy test 8
+// try to deploy test 9
 func main() {
 	http.HandleFunc("/", CityHandler)
 	http.HandleFunc("/cities.json", CityHandler)
